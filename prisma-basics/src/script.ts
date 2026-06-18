7e3d5558a4386ba5872320d039dd6f4c001fe8d2
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-06-19 02:11:55 +0600

    added script.ts

---
+++ prisma-basics/src/script.ts
@@ -0,0 +1,40 @@
+import { prisma } from "./lib/prisma";
+
+async function main() {
+  // Create a new user with a post
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice1",
+      email: "alice1@prisma.io",
+      posts: {
+        create: {
+          title: "Hello World1",
+          content: "This is my second post!",
+          published: true,
+        },
+      },
+    },
+    include: {
+      posts: true,
+    },
+  });
+  console.log("Created user:", user);
+
+  // Fetch all users with their posts
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.log("All users:", JSON.stringify(allUsers, null, 2));
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
