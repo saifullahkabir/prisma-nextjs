a18270a0f7d0084a458d7583193df12233a35060
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-06-22 20:45:51 +0600

    added users & profile model

--- blog-app-server/src/server.ts
+++ blog-app-server/src/server.ts
@@ -7,14 +7,14 @@ const PORT = config.port;
 
 async function main() {
     try {
-        // await prisma.$connect();
+        await prisma.$connect();
         console.log("Connected to the database successfully.");
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         })
     } catch (error) {
         console.log("Error starting the server:", error);
-        // await prisma.$disconnect();
+        await prisma.$disconnect();
         process.exit(1);
     }
 }
