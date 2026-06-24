5bd52c03bf45cc66a35da61ab25db65c0e90a87d
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-06-24 15:48:29 +0600

    defining send response function

--- blog-app-server/src/modules/user/user.controller.ts
+++ blog-app-server/src/modules/user/user.controller.ts
@@ -2,15 +2,15 @@ import HttpStatus from "http-status";
 import { NextFunction, Request, Response } from "express";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
-
+import { sendResponse } from "../../utils/sendResponse";
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
     const user = await userService.registerUserIntoDB(payload);
 
-    res.status(HttpStatus.CREATED).json({
-      succss: true,
+    sendResponse(res, {
+      success: true,
       statusCode: HttpStatus.CREATED,
       message: "User registered successfully",
       data: { user },
