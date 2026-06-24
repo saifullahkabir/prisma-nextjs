076fccba3e38f142bcc25ad0f9ce0cf82295f3fa
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-06-24 18:51:02 +0600

    implementing jwt for access token & refresh token

---
+++ blog-app-server/src/utils/jwt.ts
@@ -0,0 +1,16 @@
+import { JwtPayload, SignOptions } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
+const createToken = (
+  payload: JwtPayload,
+  secret: string,
+  expiresIn: SignOptions,
+) => {
+  const token = jwt.sign(payload, secret, { expiresIn } as SignOptions);
+
+  return token;
+};
+
+export const jwtUtils = {
+  createToken,
+};
