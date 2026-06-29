c41ec76e3fd5381ce32d92e3869a2d47175c09b3
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-06-29 22:25:08 +0600

    implementing transaction & rollback in getPostById api

--- blog-app-server/src/modules/post/post.service.ts
+++ blog-app-server/src/modules/post/post.service.ts
@@ -27,49 +27,53 @@ const getAllPosts = async () => {
 };
 
 const getPostById = async (postId: string) => {
-  await prisma.post.update({
-    where: {
-      id: postId,
-    },
-    data: {
-      views: {
-        increment: 1,
+  const result = await prisma.$transaction(async (tx) => {
+    await tx.post.update({
+      where: {
+        id: postId,
       },
-    },
+      data: {
+        views: {
+          increment: 1,
+        },
+      },
+    });
+
+    // throw new Error("fake error")
+
+    const post = await tx.post.findUniqueOrThrow({
+      where: {
+        id: postId,
+      },
+
+      include: {
+        author: {
+          omit: {
+            password: true,
+          },
+        },
+
+        comments: {
+          where: {
+            status: CommentStatus.APPROVED,
+          },
+
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+
+        _count: {
+          select: {
+            comments: true,
+          },
+        },
+      },
+    });
+    return post;
   });
 
-
-  const post = await prisma.post.findUniqueOrThrow({
-    where: {
-      id: postId,
-    },
-
-    include: {
-      author: {
-        omit: {
-          password: true,
-        },
-      },
-
-      comments: {
-        where: {
-          status: CommentStatus.APPROVED,
-        },
-
-        orderBy: {
-          createdAt: "desc",
-        },
-      },
-
-      _count: {
-        select: {
-          comments: true,
-        },
-      },
-    },
-  });
-
-  return post;
+  return result;
 };
 
 const updatePost = async (
