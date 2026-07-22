1fd4a43bc39420bab8da13aac504d19839ccea1d
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-07-23 04:44:49 +0600

    Handling Auth Routes In Proxy File

--- blog-app-client/proxy.ts
+++ blog-app-client/proxy.ts
@@ -1,12 +1,41 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const AUTH_ROUTES = ["/login", "/register"];
+
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  console.log(pathname);
-  console.log(request);
-  console.log("Proxy");
+
+  //   const cookieStore = await cookies();
+  //   const accessToken = cookieStore.get("accessToken")?.value;
+
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
+
+  let userRole = null;
+
+  if (decodedToken) {
+    userRole = decodedToken.role;
+  }
+
+  //* user is logged in and trying to access login or register page, redirect to dashboard or home page
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "USER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    } else if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
   //   return NextResponse.redirect(new URL("/", request.url));
   return NextResponse.next();
 }
